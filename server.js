3e144a0609d0cf19361141fcce39672492df0ae1
Ty Aponte <aaustinsk8@aol.com>
2023-01-23 02:46:47 -0800

    oopsie

--- server.js
+++ server.js
@@ -3,6 +3,22 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
+
+// mongoose.connect(process.env.DB_URL);
+
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect(process.env.DB_URL);
+}
+
+
+const db=mongoose.connection;
+db.on('error',console.error.bind(console, 'connection error'));
+db.once('open',function() {
+  console.log('mongoose is connected');
+});
 
 const app = express();
 app.use(cors());
@@ -14,5 +30,7 @@ app.get('/test', (request, response) => {
   response.send('test request received')
 
 })
+
+
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
