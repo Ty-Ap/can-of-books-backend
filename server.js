b9c47a607940b86c60e8769c282c7691e323c113
Joe Davitt <joseph.j.davitt@gmail.com>
2023-01-24 00:43:09 +0000

    add book model and schema

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const Book = require('./models/book.js')
 
 // mongoose.connect(process.env.DB_URL);
 
@@ -14,9 +15,9 @@ async function main() {
 }
 
 
-const db=mongoose.connection;
-db.on('error',console.error.bind(console, 'connection error'));
-db.once('open',function() {
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error'));
+db.once('open', function () {
   console.log('mongoose is connected');
 });
 
@@ -25,12 +26,34 @@ app.use(cors());
 
 const PORT = process.env.PORT || 3002;
 
+
+app.get('/books', getBooks);
+
+async function getBooks(request, response, next){
+  try {
+    let allBooks = await Book.find({});
+
+    response.status(200).send(allBooks);
+
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+}
+
 app.get('/test', (request, response) => {
 
   response.send('test request received')
 
 })
 
+app.get('*', (request,response) => {
+  response.status(404).send('not available');
+});
+
+app.use((error, request, response, next) => {
+  response.status(500).send(error.message);
+});
 
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
