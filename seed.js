ed30a8128ceeb7bf8047a4f5988eac76bbd0c7da
Joe Davitt <joseph.j.davitt@gmail.com>
2023-01-24 01:03:56 +0000

    fix seed

--- seed.js
+++ seed.js
@@ -11,39 +11,35 @@ async function seed() {
   // title: {type: String, required: true},
   // description: {type: String, required: true},
   // status: {type: Boolean, required: true},
-  // location: {type: String, required: true},
 
   await Book.create({
     title: 'The Neverending Story', 
-    description: '',
+    description: 'A neverending story',
     status: true,
-    location: ''
   });
 
   console.log('NO ARTAX, YOUVE GOTTA GET UP');
 
   await Book.create({
     title: 'Moby Dick',
-    description: '',
+    description: 'hunting a whale',
     status: true,
-    location: ''
   });
 
   console.log('TOWARDS THEE I ROLL. THOU ALL-DESTROYING BUT UNCONQUERING WHALE; TO THE LAST I GRAPPLE WITH THEE;FROM HELLS HEART I STAB AT THEE, ');
 
   await Book.create({
     title: '20,000 Leagues Under the Sea',
-    description: '',
+    description: 'we going deep',
     status: true,
-    location: ''
   });
   
   console.log('We must brave human laws, but we cannot resist natural ones');
 
   await Book.create({
     title: 'the Dark Tower',
-    status: '',
-    location: '',
+    description: 'go then, there are other worlds than these.',
+    status: true,
   })
 
   console.log('idk, its a tower and its dark. i didnt read this one');
